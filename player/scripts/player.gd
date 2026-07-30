b6a0438d142c577b3967177572eb3216bd9440ed
ykr <1045607920@qq.com>
2026-07-30 23:10:31 +0800

    init

---
+++ player/scripts/player.gd
@@ -0,0 +1,17 @@
+class_name Player extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _process(_delta: float) -> void:
+	
+	pass
+
+func _physics_process(_delta: float) -> void:
+	velocity.x = 0
+	
+	velocity.y = velocity.y + 980 * _delta
+	move_and_slide()
+	pass
+	
